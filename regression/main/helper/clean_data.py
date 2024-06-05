a589c2546ff9b7ceb85f25b7020904cc0b283a5a
Halef Michel <halefmichel@usp.br>
2024-06-05 03:28:48 -0300

    [main] new CBO data, was add the date column

--- regression/main/helper/clean_data.py
+++ regression/main/helper/clean_data.py
@@ -1,12 +1,12 @@
 # Open the CSV file in read mode
-with open('../../../resources/clean-data/main_rent_merge_2018ith_idh_wo_duplicates_2024_05_05.csv', 'r') as file:
+with open('/Users/karimmbk/Documents/halef-thesis/social-indicators/indicators-SP.csv', 'r') as file:
     # Read the contents of the file
     contents = file.read()
 
 # Replace all commas with periods in the contents
-contents = contents.replace('.', ',')
+contents = contents.replace(',', '.')
 
 # Open the CSV file in write mode
-with open('../../../resources/clean-data/main_rent_merge_2018ith_idh_wo_duplicates_2024_05_05_out.csv', 'w') as file:
+with open('/Users/karimmbk/Documents/halef-thesis/social-indicators/indicators-SP(1).csv', 'w') as file:
     # Write the modified contents back to the file
     file.write(contents)
