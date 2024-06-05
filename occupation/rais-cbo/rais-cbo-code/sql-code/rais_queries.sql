61c84df44780906bfa1c629901728b7093625fa2
Halef Michel <halefmichel@usp.br>
2024-06-05 10:50:45 -0300

    feat: add file

--- occupation/rais-cbo/rais-cbo-code/sql-code/rais_queries.sql
+++ occupation/rais-cbo/rais-cbo-code/sql-code/rais_queries.sql
@@ -17,30 +17,40 @@ GROUP BY r."CBO Ocupação 2002", r."Distritos SP"
 ORDER BY "Distritos SP", "CBO Ocupação 2002";
 
 
-SELECT teleworkable_score_COD."Ano",
-       teleworkable_score_COD."trimestre",
-       final_translator."CBO CÓDIGO",
-       final_translator."CBO TÍTULO",
-       final_translator."PNAD CONTÍNUA _ COD(CÓDIGO)",
-       final_translator."PNAD CONTÍNUA _COD(TÍTULO)",
-       CBO."Distritos SP",
-       CBO."total de profissionais",
-       teleworkable_score_COD."teleworkable_score"
-FROM (SELECT *
-      FROM CBO_2018_Distritos
-      UNION ALL
-      SELECT *
-      FROM CBO_2019_Distritos
-      UNION ALL
-      SELECT *
-      FROM CBO_2020_Distritos
-      UNION ALL
-      SELECT *
-      FROM CBO_2021_Distritos) AS CBO
-         LEFT JOIN final_translator ON CBO."id CBO 2002" = final_translator."CBO CÓDIGO"
-         LEFT JOIN teleworkable_score_COD
-                   ON final_translator."PNAD CONTÍNUA _ COD(CÓDIGO)" = teleworkable_score_COD."V4010" AND
-                      teleworkable_score_COD."Ano" = CBO."Year"
-WHERE final_translator."CBO CÓDIGO" IS NOT NULL
-  AND CBO."total de profissionais" > 0
-GROUP BY CBO."Distritos SP", teleworkable_score_COD."Ano";
+/* Query to get the telework ability in each district of São Paulo city by year and quarter in the years 2018, 2019, 2020 and 2021 */
+select "Distritos SP",
+       Ano,
+       Trimestre,
+       sum(cast("total de profissionais" as integer))           as total_de_profissionais,
+       round(avg(cast(teleworkable_score as real)), 3)          as avg_teleworkable_score,
+       round(sum(cast("total de profissionais" as integer) * cast(teleworkable_score as real)) /
+             sum(cast("total de profissionais" as integer)), 3) as weighted_avg_teleworkable_score
+from (SELECT teleworkable_score_COD."Ano",
+             teleworkable_score_COD."trimestre",
+             final_translator."CBO CÓDIGO",
+             final_translator."CBO TÍTULO",
+             final_translator."PNAD CONTÍNUA _ COD(CÓDIGO)",
+             final_translator."PNAD CONTÍNUA _COD(TÍTULO)",
+             CBO."Distritos SP",
+             CBO."total de profissionais",
+             teleworkable_score_COD."teleworkable_score"
+      FROM (SELECT *
+            FROM CBO_2018_Distritos
+            UNION ALL
+            SELECT *
+            FROM CBO_2019_Distritos
+            UNION ALL
+            SELECT *
+            FROM CBO_2020_Distritos
+            UNION ALL
+            SELECT *
+            FROM CBO_2021_Distritos) AS CBO
+               LEFT JOIN final_translator ON CBO."id CBO 2002" = final_translator."CBO CÓDIGO"
+               LEFT JOIN teleworkable_score_COD
+                         ON final_translator."PNAD CONTÍNUA _ COD(CÓDIGO)" = teleworkable_score_COD."V4010" AND
+                            teleworkable_score_COD."Ano" = CBO."Year"
+      WHERE final_translator."CBO CÓDIGO" IS NOT NULL
+        AND CBO."total de profissionais" > 0) t
+where Ano IS NOT NULL
+group by "Distritos SP", Ano, Trimestre
+order by "Distritos SP", Ano, Trimestre;
