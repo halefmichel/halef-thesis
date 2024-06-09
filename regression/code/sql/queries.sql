f38a8f7a297feaab6107ce6afb4452c76d94ffce
Halef Michel <halefmichel@usp.br>
2024-06-09 10:24:41 -0300

    feat: formated data

--- regression/code/sql/queries.sql
+++ regression/code/sql/queries.sql
@@ -2,9 +2,9 @@ select avg(price_real_month)           as avg_rent,
        avg(cast(teleworkable as real)) as avg_teleworkable,
        year,
        district
-from rent_2018
+from rent_2018_2021_out
 group by year, district, cast(teleworkable as real);
 
 
 select *
-from rent_2018;
+from rent_2018_2021_out;
