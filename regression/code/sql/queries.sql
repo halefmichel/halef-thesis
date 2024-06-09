50223a8ce369929130d08451713b78f491c77842
Halef Michel <halefmichel@usp.br>
2024-06-09 19:29:46 -0300

    [main] delete file and change on regression code

--- regression/code/sql/queries.sql
+++ regression/code/sql/queries.sql
@@ -1,10 +1,18 @@
-select avg(price_real_month)           as avg_rent,
-       avg(cast(teleworkable as real)) as avg_teleworkable,
-       year,
-       district
-from rent_2018_2021_out
-group by year, district, cast(teleworkable as real);
+SELECT year,
+       id,
+       nm_distrit,
+       ROUND(AVG(price_real_month), 3)           AS avg_rent,
+       ROUND(AVG(CAST(teleworkable AS REAL)), 3) AS avg_teleworkable
+FROM rent_2018_2021_out
+GROUP BY year, nm_distrit
+ORDER BY nm_distrit;
 
-
-select *
-from rent_2018_2021_out;
+SELECT id,
+       nm_distrit,
+       ROUND(AVG(inequality_meter), 3)                                                                               AS inequality_meter,
+       ROUND(AVG(CAST(delta_cbd_farialima AS REAL)), 3)                                                              AS avg_dist_cbd,
+       ROUND(AVG(inequality_meter) * (1 / AVG(CAST(delta_cbd_farialima AS REAL))), 3)                                AS score,
+       RANK() OVER (ORDER BY ROUND(AVG(inequality_meter) * (1 / AVG(CAST(delta_cbd_farialima AS REAL))), 3) DESC)    AS rank
+FROM rent_2018_2021_out
+GROUP BY nm_distrit
+ORDER BY rank;
